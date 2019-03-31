e278d90022663a9b5db35f91611d851cb4e0306c
javidpoornasir <javidp.sf@gmail.com>
2019-03-31 01:53:44 -0700

    cleanup

--- LOCAL NOTIFICATION/AppDelegate.swift
+++ LOCAL NOTIFICATION/AppDelegate.swift
@@ -14,168 +14,111 @@ import FirebaseInstanceID // support the token that firebase provides to identif
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
-
+    
     var window: UIWindow?
+    
     
     
     // MessagingDelegate - a protocol to handle token update or data message delivery from FCM
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
         FirebaseApp.configure()
-        
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
-            
             if error != nil {
                 // something bad happened
             } else {
                 UNUserNotificationCenter.current().delegate = self // the delegate methods will be handled by this class is what this means
                 Messaging.messaging().delegate = self
-                
                 print("GRANTED NOTIFICATIONS: \(granted)")
-                
             }
         }
-        
         DispatchQueue.main.async {
             // Must request authorization that we can receive any type of push notification
             //UIApplication.shared
-                application.registerForRemoteNotifications()
-        }
-        return true
+            application.registerForRemoteNotifications()
+        }; return true
     }
     
-    
-    
-    
-    func connectToFirebaseConnectionMgr() {
-        // call this immediately after app becomes active
+    func connectToFirebaseConnectionMgr() {  // call this immediately after app becomes active
         Messaging.messaging().shouldEstablishDirectChannel = true
     }
     
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         // Notification now displays when app's in foreground
         completionHandler([.alert, .sound])
     }
     
-    
-    
-    
     // Reacts to tapping on the notifctn
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         // We have access to the notf. so we can access the identifier
-        
         if response.notification.request.identifier == "testIdentifier" {
-                    print("HANDLING NOTF. WITH THE IDENTIFIER 'testIdentifier'")
-                    // We can launch a specific screen from here
-            
-                    // We also access the payload of what was sent in the push notification in here and here's some code to help us parse the notification
+            print("HANDLING NOTF. WITH THE IDENTIFIER 'testIdentifier'")
+            // We can launch a specific screen from here
+            // We also access the payload of what was sent in the push notification in here and here's some code to help us parse the notification
             if let notification = response.notification.request.content.userInfo as? [String: AnyObject] {
-                
                 let message = parseRemoteNotification(notification: notification)
-                
                 print(message as Any)
-            }
-            
+            } 
             completionHandler()
         }
         
         // HERE'S ANOTHER POSSIBILITY OF WHAT COULD BE DONE INSTEAD OF WHAT'S ABOVE - THIS WOULD MAKE THE ACTION BUTTONS WORK
         
         // THIS WOULD HELP US TO SHOW WHICH BOXING EVENTS OR CALENDAR EVENTS HAVE HAD REMINDERS SET UP IN THE FORM OF PUSH NOTF.
-//        https://www.youtube.com/watch?v=e7cTZ4Tp25I
-//        let foodItem = Food(context: persistentContainer.viewContext)
-//        foodItem.added = NSDate()
-//
-//        if response.actionIdentifier == "addFruit" {
-//            foodItem.type = "Fruit"
-//
-//        } else { // veggie
-//
-//            foodItem.type = "Vegetable"
-//        }
-//
-//        self.saveContext()
-//        scheduleNotification() // this would be here if we were to put this func in this class instead of ViewController.swift
-//         completionHandler()
+        //        https://www.youtube.com/watch?v=e7cTZ4Tp25I
+        //        let foodItem = Food(context: persistentContainer.viewContext)
+        //        foodItem.added = NSDate()
+        //        if response.actionIdentifier == "addFruit" {
+        //            foodItem.type = "Fruit"
+        //        } else { // veggie
+        //            foodItem.type = "Vegetable"
+        //        }
+        //        self.saveContext()
+        //        scheduleNotification() // this would be here if we were to put this func in this class instead of ViewController.swift
+        //         completionHandler()
     }
-    
-    
-    
     
     private func parseRemoteNotification(notification: [String: AnyObject]) -> String? {
         if let aps = notification["aps"] as? [String: AnyObject] {
             let alert = aps["alert"] as? String
             return alert
-        }
-        return nil
+        }; return nil
     }
     
-
-    
-    
-    
-    // PUSH NOTIFICATIONS
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) { // Callback for receiving device token
         let token = deviceToken.map{ String(format: "%02.2hhx", $0) }.joined()
         print("PRINTING THE DEVICE TOKEN: \(token)")
-        
         // Device token doesn't usually change but sometimes it does so we need to have a service to make sure we can update it
     }
-    
-    
-    
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) { // Called if device token not received
     }
     
     
-
+    
     // FIREBASE
     
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
-        // properly handling a refreshed token in Swift 4 may be more difficult than it appears
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) { // properly handling a refreshed token in Swift 4 may be more difficult than it appears
         let newToken = InstanceID.instanceID().token()
-        connectToFirebaseConnectionMgr() // called if we get a new token so it can reset itself
+        connectToFirebaseConnectionMgr()                        // called if we get a new token so it can reset itself
     }
     
     func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
-        
     }
     
     
-    
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        
-        
-        // break our connection to FCM (firebase connection mgr)
+    func applicationDidEnterBackground(_ application: UIApplication) {  // break our connection to FCM (firebase connection mgr)
         Messaging.messaging().shouldEstablishDirectChannel = false
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        
         connectToFirebaseConnectionMgr() // FCM
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
+    
     
     
 }
